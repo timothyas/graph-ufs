ac1c906a9d8d291e18d86eb74b010d4f6ae07a5f
timothyas <tim.smith@noaa.gov>
2024-08-23 15:39:16 -0500

    updates to get this working

--- scripts/forecast_member.py
+++ scripts/forecast_member.py
@@ -1,7 +1,20 @@
-"""TODO:
-Notes:
-    For the first cycle, we want to read fhr06 and fhr09
-    After that we will just make 6 hour predictions with the ML models, so we just want fhr03 and fhr06
+"""Standalone script to run GraphUIFS forecast
+
+Example usage:
+
+    $ python forecast.py \
+            --ensemble_member=10 \
+            --previous_cycle=2021120106 \
+            --experiment_directory=/work2/noaa/gsienkf/whitaker/C96L127_graphufs_psonlyiau \
+            --fhr_ics=6,9
+
+For help with the inputs:
+
+    $ python forecast.py --help
+
+Note:
+    For the first cycle, we want to read fhr06 and fhr09 (--fhr_ics=6,9)
+    After that we will just make 6 hour predictions with the ML models, so we just want fhr03 and fhr06 (--fhr_ics=3,6)
 """
 import os
 import sys
@@ -27,12 +40,13 @@ from graphcast import (
     graphcast,
     normalization as graphcastnorm,
     rollout,
+    solar_radiation,
 )
 
-ENSEMBLE_SIZE = flags.DEFINE_integer(
-    "ensemble_size",
+ENSEMBLE_MEMBER = flags.DEFINE_integer(
+    "ensemble_member",
     None,
-    help="Size of the ensemble, e.g. 80",
+    help="Ensemble member ID to propagate",
 )
 PREVIOUS_CYCLE = flags.DEFINE_string(
     "previous_cycle",
@@ -47,16 +61,20 @@ EXPERIMENT_DIRECTORY = flags.DEFINE_string(
 FHR_ICS = flags.DEFINE_list(
     "fhr_ics",
     None,
-    help="Forecast hours to grab for initial conditions. It will be [6,9] when initializing from a single-IC model (e.g. UFS), and [3, 6] when initializing from a 2-IC ML model",
+    help="Forecast hours to grab for initial conditions. It will be --fhr_ics=6,9 when initializing from a single-IC model (e.g. UFS), and --fhr_ics=3,6 when initializing from a 2-IC ML model",
+)
+MODEL_WEIGHTS = flags.DEFINE_string(
+    "model_weights",
+    "/work2/noaa/gsienkf/timsmith/model-weights/graphufs_p1v1.npz",
+    help="Path to model weights to be used with GraphCast code base",
 )
 
-_norm_urls = {
-    "mean": "gs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree-subsampled/03h-freq/zarr/fv3.statistics.1993-2019/mean_by_level.zarr",
-    "std": "gs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree-subsampled/03h-freq/zarr/fv3.statistics.1993-2019/stddev_by_level.zarr",
-    "stddiff": "gs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree-subsampled/03h-freq/zarr/fv3.statistics.1993-2019/diffs_stddev_by_level.zarr",
-}
 
-_data_url = "gs://noaa-ufs-gefsv13replay/ufs-hr1/0.25-degree-subsampled/03h-freq/zarr/fv3.zarr"
+_norm_paths = {
+    "mean": "/work2/noaa/gsienkf/timsmith/replay-normalization-statistics/0.25-degree-subsampled/03h-freq/fv3.statistics.1993-2019/mean_by_level.nc",
+    "std": "/work2/noaa/gsienkf/timsmith/replay-normalization-statistics/0.25-degree-subsampled/03h-freq/fv3.statistics.1993-2019/stddev_by_level.nc",
+    "stddiff": "/work2/noaa/gsienkf/timsmith/replay-normalization-statistics/0.25-degree-subsampled/03h-freq/fv3.statistics.1993-2019/diffs_stddev_by_level.nc",
+}
 
 class SimpleFormatter(logging.Formatter):
     def format(self, record):
@@ -131,11 +149,14 @@ def _datetime2cftime(datetime):
 def postprocess(xds, last_ic_datetime):
     """Take prediction from GraphCast and prepare it for FV3/DA"""
 
-    xds = xds.rename({
+    rename = {
         "level": "pfull",
         "lat": "grid_yt",
         "lon": "grid_xt",
-    })
+    }
+    for key, val in rename.items():
+        if key in xds:
+            xds = xds.rename({key: val})
 
     xds = xds.squeeze().drop_vars("batch")
     xds = xds.rename({"time": "ftime"})
@@ -162,8 +183,13 @@ def store_result(xds, prefix_mapper, cycle, member, experiment_directory):
             fname = f"{outer_dir}/{prefix}_{cycle}_fhr{fhr:02d}_mem{member:03d}"
             tmpds = xr.Dataset({
                 key: result[key]
-                for key in result.data_vars if prefix_mapper[key] == prefix
-            })
+                for key in result.data_vars if prefix_mapper[key] == prefix or key == "pressfc"
+            },
+            attrs=xds.attrs["file_attrs"][prefix.replace("sfg","sanl")]
+            )
+            if "ak" in xds.attrs and prefix == "sfg":
+                tmpds.attrs["ak"] = xds.ak
+                tmpds.attrs["bk"] = xds.bk
             tmpds.to_netcdf(fname)
             logging.info(f"Stored {fname}")
 
@@ -196,15 +222,69 @@ def get_empty_targets(lead_times, template):
     container = container.set_coords("datetime")
     return container
 
+def get_forcings(lead_times, ids):
+    """This is a hack for now, since the forcings component is a bit wonky"""
+
+    # setup container for the forcings
+
+    fds = xr.Dataset()
+    for key in ["lat", "lon"]:
+        fds[key] = ids[key]
+
+    time = [pd.Timedelta(t) for t in lead_times]
+    datetime = [pd.Timestamp(ids.datetime.values[-1]) + pd.Timedelta(t) for t in lead_times]
+    fds["time"] = xr.DataArray(
+        time,
+        coords={"time": time},
+        dims="time",
+    )
+    fds["datetime"] = xr.DataArray(
+        datetime,
+        coords=fds.time.coords,
+    )
+    fds = fds.set_coords("datetime")
+
+    # First, use code in GraphCast repo to compute downward shortwave radiation
+    # note that this is numerically identical to Replay, especially given that
+    # this will be converted to half precision
+    fds["dswrf_avetoa"] = solar_radiation.get_toa_incident_solar_radiation_for_xarray(
+        fds,
+        integration_period="3h",
+    )
+    # GraphCast computes temporal accumulation of solar radiation
+    # whereas our model expects the instantaneous flux, averaged over the same time period
+    fds["dswrf_avetoa"] = fds["dswrf_avetoa"] / (3600 * 3)
+
+    # Now get hgtsfc
+    fds["hgtsfc"] = ids["hgtsfc"].isel(time=0, drop=True)
+    fds["hgtsfc"] = fds["hgtsfc"].broadcast_like(fds["time"])
+
+    # Now for the real hack:
+    # land is a time varying quantity, and we falsely prescribe it as forcings
+    # during prediction time
+    # for now, just take the last "land" value from the initial condition dataset
+    # and keep it static going forward
+    fds["land"] = ids["land"].isel(time=-1, drop=True)
+    fds["land"] = fds["land"].broadcast_like(fds["time"])
+    return fds
+
+
 
 def get_model_inputs(xds, member, lead_times, task_config):
     """Transform initial conditions from sanl/bfg files to inputs, targets_template, forcings
     so they're ready to be passed to the model
     """
 
-    # append template for the target lead times
-    targets_template = get_empty_targets(lead_times, xds)
-    xds = xr.concat([xds, targets_template], dim="time")
+    # append template and forcings for the target lead times
+    targets_template = get_empty_targets(lead_times, xds[[x for x in task_config.target_variables if x in xds]])
+    forcings = get_forcings(lead_times, xds[[x for x in task_config.forcing_variables if x in xds]])
+    future = xr.merge([targets_template, forcings])
+    xds = xr.concat([xds, future], dim="time")
+
+    for key in xds.data_vars:
+        isnans = np.isnan(xds[key]).any(["lat", "lon"])
+        if isnans.any().values:
+            logging.warning(f"{key} found NaNs: \n{isnans}\n")
 
     # transform into inputs, targets_template, forcings
     levels = list(
@@ -238,11 +318,37 @@ def preprocess(xds, cycle, relative_time, task_config, prefix):
     """
 
     # select variables
+    is_from_ufs = "o3mr" in xds
     xds = xds[[x for x in get_all_variables(task_config) if x in xds]]
 
     # select vertical levels
     if "pfull" in xds.dims:
+        pfull = xds.pfull.values
         xds = xds.sel(pfull=list(task_config.pressure_levels), method="nearest")
+
+        # get the ak/bk coefficients, above each level
+        if is_from_ufs:
+
+            # instead of complicated estimate below, use indices suggested by Jeff
+            indices = [ 28, 40, 47, 53, 56, 61, 65, 73, 79, 83, 94, 104,115,127]
+            #indices = [list(pfull).index(x) for x in xds.pfull.values]
+            #indices.sort()
+            ## now append the very bottom (surface)
+            #indices.append(indices[-1]+1)
+
+            ## now for the interfaces in the middle, get an interface somewhere between the
+            ## subsampled cell centers
+            ## do this while preserving the top coefficient, and the bottom two coefficents
+            ## closest to the surface
+            #middle = list(int(np.mean([x,y])) for x, y in zip(indices[:-3], indices[1:-2]))
+            #indices = [indices[0]] + middle + indices[-2:]
+
+            xds.attrs["ak"] = xds.ak[indices]
+            xds.attrs["bk"] = xds.bk[indices]
+
+        else:
+            xds.attrs["ak"] = xds.ak
+            xds.attrs["bk"] = xds.bk
 
     # convert datetime format from cftime to np.datetime64
     xds = xds.rename({"time": "cftime"})
@@ -272,6 +378,9 @@ def preprocess(xds, cycle, relative_time, task_config, prefix):
     # add file prefix to these variables in order to store result in separate netcdf files
     for key in xds.data_vars:
         xds[key].attrs["file_prefix"] = prefix.replace("sanl", "sfg")
+
+    # finally, load this into memory
+    xds = xds.load();
     return xds
 
 
@@ -294,15 +403,18 @@ def open_dataset(
         xds (xr.Dataset): with sanl/bfg combined, taking surface pressure from sanl since that's updated by the DA
     """
 
+    attrs = {}
     dslist1 = []
     for fhr in fhrs:
         dslist2 = []
-        for prefix in ["sfg", "bfg"]:
+        for prefix in ["sanl", "bfg"]:
             tmp = xr.open_dataset(
                 f"{experiment_directory}/{cycle}/{prefix}_{cycle}_fhr{fhr:02d}_mem{member:03d}",
             )
-            if prefix == "bfg":
+            if prefix == "bfg" and "pressfc" in tmp:
                 tmp = tmp.drop_vars("pressfc")
+            if prefix not in attrs:
+                attrs[prefix] = tmp.attrs.copy()
 
             tmp = preprocess(
                 tmp,
@@ -316,6 +428,7 @@ def open_dataset(
         dslist1.append(xr.merge(dslist2))
 
     xds = xr.concat(dslist1, dim="time")
+    xds.attrs["file_attrs"] = attrs.copy()
     return xds
 
 
@@ -324,7 +437,7 @@ def load_normalization(task_config):
 
     normalization = dict()
     for key in ["mean", "std", "stddiff"]:
-        this_norm = xr.open_zarr(_norm_urls[key], storage_options={"token":"anon"})
+        this_norm = xr.open_dataset(_norm_paths[key])
         this_norm = this_norm[[x for x in get_all_variables(task_config) if x in this_norm]]
         this_norm = this_norm.sel(pfull=list(task_config.pressure_levels), method="nearest")
         this_norm = this_norm.rename({"pfull": "level"})
@@ -403,21 +516,14 @@ def predict(
     )
     return predictions
 
-
-
-
 def main(argv):
-    setup_log()
-    logging.info("Reading weights")
-    params, model_config, task_config = load_checkpoint("./graphufs_p1v1.npz")
 
-    logging.info("Reading normalization")
-    normalization = load_normalization(task_config)
-
+    # Inputs and some checking
     previous_cycle = PREVIOUS_CYCLE.value
-    ensemble_size = ENSEMBLE_SIZE.value
+    member = ENSEMBLE_MEMBER.value
     experiment_directory = EXPERIMENT_DIRECTORY.value
     fhr_ics = tuple(int(x) for x in FHR_ICS.value)
+    model_weights = MODEL_WEIGHTS.value
 
     assert len(previous_cycle) == 10, "previous_cycle must be a 10 character long string in format YYYYMMDDHH"
     expected = _timestamp2cycle(_cycle2timestamp(previous_cycle))
@@ -425,48 +531,65 @@ def main(argv):
 
     next_cycle = _timestamp2cycle( _cycle2timestamp(previous_cycle) + pd.Timedelta(hours=6) )
 
+    setup_log()
+    logging.info(f"Reading weights from: {model_weights}")
+    params, model_config, task_config = load_checkpoint(model_weights)
+
+    logging.info("Reading normalization")
+    normalization = load_normalization(task_config)
+
     # Note that all of the code below, including postprocess, assumes
     # we are working one ensemble member at a time
-    for member in range(1, ensemble_size+1):
-        logging.info(f"Reading initial conditions from cycle: {previous_cycle}, fhrs: {fhr_ics}, member: {member}")
-        xds = open_dataset(
-            cycle=previous_cycle,
-            member=member,
-            task_config=task_config,
-            fhrs=fhr_ics,
-            experiment_directory=experiment_directory,
-        )
-        prefix_mapper = {key: xds[key].attrs["file_prefix"] for key in xds.data_vars}
 
-        logging.info("Preparing initial conditions for GraphCast code")
-        inputs, targets_template, forcings = get_model_inputs(
-            xds,
-            member=member,
-            lead_times=["3h", "6h"],
-            task_config=task_config,
-        )
+    logging.info(f"Reading initial conditions from cycle: {previous_cycle}, fhrs: {fhr_ics}, member: {member}")
+    xds = open_dataset(
+        cycle=previous_cycle,
+        member=member,
+        task_config=task_config,
+        fhrs=fhr_ics,
+        experiment_directory=experiment_directory,
+    )
+    prefix_mapper = {key: xds[key].attrs["file_prefix"] for key in xds.data_vars}
 
-        logging.info(f"Starting GraphUFS Prediction for cycle: {next_cycle}, member: {member}")
-        prediction = predict(
-            params=params,
-            inputs=inputs,
-            targets_template=targets_template,
-            forcings=forcings,
-            model_config=model_config,
-            task_config=task_config,
-            normalization=normalization,
-        )
+    logging.info("Preparing initial conditions for GraphCast code")
+    inputs, targets_template, forcings = get_model_inputs(
+        xds,
+        member=member,
+        lead_times=["3h", "6h"],
+        task_config=task_config,
+    )
 
-        logging.info("Postprocessing Prediction for DA")
-        prediction = postprocess(prediction, last_ic_datetime=xds["datetime"].values[1])
+    logging.info(f"Starting GraphUFS Prediction for cycle: {next_cycle}, member: {member}")
+    prediction = predict(
+        params=params,
+        inputs=inputs,
+        targets_template=targets_template,
+        forcings=forcings,
+        model_config=model_config,
+        task_config=task_config,
+        normalization=normalization,
+    )
 
-        store_result(
-            prediction,
-            prefix_mapper=prefix_mapper,
-            cycle=next_cycle,
-            member=member,
-            experiment_directory=experiment_directory,
-        )
+    logging.info("Postprocessing Prediction for DA")
+    prediction = postprocess(prediction, last_ic_datetime=xds["datetime"].values[1])
+    prediction.attrs = xds.attrs.copy()
+
+    # take the forcing and stick it in the prediction dataset as well
+    postforcing = postprocess(forcings, last_ic_datetime=xds["datetime"].values[1])
+    for key in postforcing.data_vars:
+        if key in xds.data_vars:
+            logging.info(f"Storing forcing[{key}] -> prediction dataset")
+            prediction[key] = postforcing[key]
+            prediction[key].attrs = xds[key].attrs.copy()
+
+
+    store_result(
+        prediction,
+        prefix_mapper=prefix_mapper,
+        cycle=next_cycle,
+        member=member,
+        experiment_directory=experiment_directory,
+    )
 
 if __name__ == "__main__":
     app.run(main)
