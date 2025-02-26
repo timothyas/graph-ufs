8c1e1003410f323dc5c9c055551249667ffe52e4
timothyas <tim.smith@noaa.gov>
2025-02-26 11:11:32 -0700

    multiple inheritance ftw

--- prototypes/gefs/OnlyOneDegree/config.py
+++ prototypes/gefs/OnlyOneDegree/config.py
@@ -1,27 +1,17 @@
 from jax import tree_util
 from jax import numpy as jnp
+from graphufs.gefs import GEFSDeviationEmulator
 from prototypes.gefs.config import BaseGEFSEmulator, _scratch
 
-class GEFSTrainer(BaseGEFSEmulator):
+class GEFSMSETrainer(BaseGEFSEmulator):
 
-    local_store_path = f"./zarr-stores"
+    local_store_path = f"./mse-loss"
 
-class GEFSPreprocessor(GEFSTrainer):
+class GEFSDeviationTrainer(GEFSDeviationEmulator, BaseGEFSEmulator):
 
-    batch_size = 64
+    local_store_path = f"./dev-loss"
 
-class GEFSPreprocessed(GEFSTrainer):
-    """The log transform has already been taken care of during preprocessing.
-    This version operates on transformed (preprocessed) data, so needs no transforms.
-
-    Note that it is a happy coincidence that we needed to create a separate compilable
-    input/output transform to satisfy jax, since we will still need those transforms (but not the xarray ones) here.
-    """
-    input_transforms = None
-    output_transforms = None
-
-
-class GEFSEvaluator(GEFSTrainer):
+class GEFSEvaluator(GEFSMSETrainer):
     wb2_obs_url = "gs://weatherbench2/datasets/era5/1959-2023_01_10-6h-240x121_equiangular_with_poles_conservative.zarr"
     target_lead_time = [f"{n}h" for n in range(3, 3*8*10+1, 3)]
     sample_stride = 9
@@ -30,21 +20,15 @@ class GEFSEvaluator(GEFSTrainer):
 
 
 tree_util.register_pytree_node(
-    GEFSTrainer,
-    GEFSTrainer._tree_flatten,
-    GEFSTrainer._tree_unflatten
+    GEFSMSETrainer,
+    GEFSMSETrainer._tree_flatten,
+    GEFSMSETrainer._tree_unflatten
 )
 
 tree_util.register_pytree_node(
-    GEFSPreprocessor,
-    GEFSPreprocessor._tree_flatten,
-    GEFSPreprocessor._tree_unflatten
-)
-
-tree_util.register_pytree_node(
-    GEFSPreprocessed,
-    GEFSPreprocessed._tree_flatten,
-    GEFSPreprocessed._tree_unflatten
+    GEFSDeviationTrainer,
+    GEFSDeviationTrainer._tree_flatten,
+    GEFSDeviationTrainer._tree_unflatten
 )
 
 tree_util.register_pytree_node(
