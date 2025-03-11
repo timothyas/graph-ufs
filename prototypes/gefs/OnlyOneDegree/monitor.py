5113a7a1fecd1fab1d0d8703e7ec011e6c1c3530
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-03-11 13:42:11 -0700

    checking in again

--- prototypes/gefs/OnlyOneDegree/monitor.py
+++ prototypes/gefs/OnlyOneDegree/monitor.py
@@ -6,45 +6,48 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 
+_project = "gefs"
+_subproject = "OnlyOneDegree"
+
 if __name__ == "__main__":
 
-    ds = xr.load_dataset("/pscratch/sd/t/timothys/graph-ufs/gefs/OnlyOneDegree/forecast-training/loss.nc")
+    for stage in ["forecast", "deviation"]:
 
-    fig, axs = plt.subplots(1,2, figsize=(8,4), constrained_layout=True)
+        ds = xr.load_dataset(f"/pscratch/sd/t/timothys/graph-ufs/{_project}/{_subproject}/{stage}-training/loss.nc")
 
-    axLR = axs[0].twinx()
-    l1 = ds.loss.plot(ax=axs[0], color="C0", label="Training Loss")
-    l2 = ds.learning_rate.plot(ax=axLR, color="gray", label="Learning Rate")
+        fig, axs = plt.subplots(1,2, figsize=(8,4), constrained_layout=True)
 
-    ds.loss_avg.plot(ax=axs[1], label="Training")
-    ds.loss_valid.plot(ax=axs[1], label="Validation")
+        axLR = axs[0].twinx()
+        l1 = ds.loss.plot(ax=axs[0], color="C0", label="Training Loss")
+        l2 = ds.learning_rate.plot(ax=axLR, color="gray", label="Learning Rate")
 
-    for ax in axs:
-        for key in ["right", "top"]:
-            ax.spines[key].set_visible(False)
-    axLR.spines["top"].set_visible(False)
+        ds.loss_avg.plot(ax=axs[1], label="Training")
+        ds.loss_valid.plot(ax=axs[1], label="Validation")
 
-    # labels and stuff
-    axs[0].set(
-        xlabel="Optimization Step",
-        ylabel="Loss Value",
-        ylim=[-.1, 2]
-    )
-    axLR.set(ylabel="Learning Rate")
-    axs[1].set(
-        xlabel="Epoch",
-        ylabel="Loss Value",
-        ylim=[-.1, 1],
-    )
-    lines = [l1[0], l2[0]]
-    axs[0].legend(
-        lines,
-        list(l.get_label() for l in lines),
-        loc="center right",
-    )
-    axs[1].legend()
+        for ax in axs:
+            for key in ["right", "top"]:
+                ax.spines[key].set_visible(False)
+        axLR.spines["top"].set_visible(False)
 
-    fig_dir = "./figures"
-    if not os.path.isdir(fig_dir):
-        os.makedirs(fig_dir)
-    fig.savefig(f"{fig_dir}/training_loss.pdf")
+        # labels and stuff
+        axs[0].set(
+            xlabel="Optimization Step",
+            ylabel="Loss Value",
+        )
+        axLR.set(ylabel="Learning Rate")
+        axs[1].set(
+            xlabel="Epoch",
+            ylabel="Loss Value",
+        )
+        lines = [l1[0], l2[0]]
+        axs[0].legend(
+            lines,
+            list(l.get_label() for l in lines),
+            loc="center right",
+        )
+        axs[1].legend()
+
+        fig_dir = "./figures"
+        if not os.path.isdir(fig_dir):
+            os.makedirs(fig_dir)
+        fig.savefig(f"{fig_dir}/training_loss_{stage}.pdf")
