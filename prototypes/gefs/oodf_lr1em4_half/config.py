d8a132f54ab29db5dedcf6cdb66d7b088c0fe9bc
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-03-14 09:36:55 -0700

    experiment with half amount of data

---
+++ prototypes/gefs/oodf_lr1em4_half/config.py
@@ -0,0 +1,49 @@
+from jax import tree_util
+from prototypes.gefs.config import BaseGEFSEmulator, _scratch
+
+class GEFSForecastTrainer(BaseGEFSEmulator):
+
+    local_store_path = f"{_scratch}/graph-ufs/gefs/oodf_lr1em4_half"
+    num_epochs = 128
+    peak_lr = 1e-4
+
+class GEFSForecastPreprocessor(GEFSForecastTrainer):
+
+    batch_size = 64
+
+class GEFSForecastPreprocessed(GEFSForecastTrainer):
+    input_transforms = None
+    output_transforms = None
+
+class GEFSForecastEvaluator(GEFSForecastTrainer):
+
+    data_url = f"{_scratch}/gefs/one-degree/forecasts.validation.zarr"
+    wb2_obs_url = "gs://gcp-public-data-arco-era5/ar/1959-2022-1h-360x181_equiangular_with_poles_conservative.zarr"
+    target_lead_time = [f"{n}h" for n in range(6, 6*4*10+1, 6)]
+    #evaluation_checkpoint_id = 64
+    evaluation_checkpoint_id = 18
+    batch_size = 32
+
+tree_util.register_pytree_node(
+    GEFSForecastTrainer,
+    GEFSForecastTrainer._tree_flatten,
+    GEFSForecastTrainer._tree_unflatten
+)
+
+tree_util.register_pytree_node(
+    GEFSForecastPreprocessor,
+    GEFSForecastPreprocessor._tree_flatten,
+    GEFSForecastPreprocessor._tree_unflatten
+)
+
+tree_util.register_pytree_node(
+    GEFSForecastPreprocessed,
+    GEFSForecastPreprocessed._tree_flatten,
+    GEFSForecastPreprocessed._tree_unflatten
+)
+
+tree_util.register_pytree_node(
+    GEFSForecastEvaluator,
+    GEFSForecastEvaluator._tree_flatten,
+    GEFSForecastEvaluator._tree_unflatten
+)
---
+++ prototypes/gefs/oodf_lr1em4_half/train.py
@@ -0,0 +1,147 @@
+import os
+import logging
+
+from mpi4py import MPI
+
+from graphufs.datasets import Dataset
+from graphufs.tensorstore import PackedDataset as TSPackedDataset, MPIBatchLoader as TSBatchLoader
+from graphufs.mpi import MPITopology
+
+from graphufs.stacked_mpi_training import (
+    init_model,
+    optimize,
+)
+
+from graphufs.optim import clipped_cosine_adamw
+from graphufs import utils
+
+from config import (
+    GEFSForecastTrainer as RemoteTrainer,
+    GEFSForecastPreprocessed as PackedTrainer,
+)
+
+
+def train(RemoteEmulator, PackedEmulator, missing_samples=None, cfg=None):
+    """
+
+    Args:
+        RemoteEmulator, PackedEmulator (graphufs.Emulator)
+        cfg (dict, optional): if provided, a dict with
+            params, state, opt_state
+    """
+
+    # initial setup
+    is_pickup = True
+    if cfg is None:
+        is_pickup = False
+        cfg = dict()
+        cfg["opt_state"] = None
+
+    cfg["topo"] = MPITopology(log_dir=f"{RemoteEmulator.local_store_path}/logs/training")
+    emulator = PackedEmulator(mpi_rank=cfg["topo"].rank, mpi_size=cfg["topo"].size)
+    remote_emulator = RemoteEmulator(mpi_rank=cfg["topo"].rank, mpi_size=cfg["topo"].size)
+
+    # data generators
+    tds = Dataset(remote_emulator, mode="training")
+    training_data = TSPackedDataset(emulator, mode="training", missing_samples=missing_samples)
+    validation_data = TSPackedDataset(emulator, mode="validation")
+
+    n_samples = len(training_data) // 2
+    training_data.inputs = training_data.inputs.isel(sample=slice(n_samples))
+    training_data.targets = training_data.targets.isel(sample=slice(n_samples))
+
+    trainer = TSBatchLoader(
+        training_data,
+        batch_size=emulator.batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=emulator.num_workers,
+        max_queue_size=emulator.max_queue_size,
+        mpi_topo=cfg["topo"],
+        rng_seed=10,
+    )
+    validator = TSBatchLoader(
+        validation_data,
+        batch_size=emulator.batch_size,
+        shuffle=True,
+        drop_last=True,
+        num_workers=emulator.num_workers,
+        max_queue_size=emulator.max_queue_size,
+        mpi_topo=cfg["topo"],
+        rng_seed=11,
+    )
+
+    logging.info("Initializing Loss Function Weights and Stacked Mappings")
+    # compute loss function weights once
+    loss_weights = emulator.calc_loss_weights(tds)
+    last_input_channel_mapping = utils.get_last_input_mapping(tds)
+
+    # initialize a random model
+    if not is_pickup:
+        logging.info("Initializing Optimizer and Parameters")
+        inputs, _ = trainer.get_data()
+        cfg["params"], cfg["state"] = init_model(
+            emulator=emulator,
+            inputs=inputs,
+            last_input_channel_mapping=last_input_channel_mapping,
+            mpi_topo=cfg["topo"],
+        )
+
+    loss_name = f"{emulator.local_store_path}/loss.nc"
+    if cfg["topo"].is_root:
+        emulator.save_checkpoint(cfg["params"], id=0)
+        if os.path.exists(loss_name):
+            os.remove(loss_name)
+
+    # setup optimizer
+    steps_in_epoch = len(trainer)
+    n_total = emulator.num_epochs * steps_in_epoch
+    n_linear = emulator.n_linear_warmup_steps
+    n_cosine = n_total - n_linear
+    optimizer = clipped_cosine_adamw(
+        n_linear=n_linear,
+        n_total=n_total,
+        peak_value=emulator.peak_lr,
+    )
+
+    logging.info(f"Starting Training with:")
+    logging.info(f"\t batch_size = {emulator.batch_size}")
+    logging.info(f"\t {len(trainer)} training steps per epoch")
+    logging.info(f"\t {len(validator)} validation steps per epoch")
+    logging.info(f"\t ---")
+    logging.info(f"\t {n_linear} linearly increasing LR steps")
+    logging.info(f"\t {n_cosine} cosine decay LR steps")
+    logging.info(f"\t {n_total} total training steps")
+
+    # training
+    for e in range(emulator.num_epochs):
+        logging.info(f"Starting epoch {e+1}")
+
+        # optimize
+        cfg["params"], loss, cfg["opt_state"] = optimize(
+            params=cfg["params"],
+            state=cfg["state"],
+            optimizer=optimizer,
+            emulator=emulator,
+            trainer=trainer,
+            validator=validator,
+            loss_weights=loss_weights,
+            last_input_channel_mapping=last_input_channel_mapping,
+            opt_state=cfg["opt_state"],
+            mpi_topo=cfg["topo"],
+        )
+
+        # save weights
+        logging.info(f"Done with epoch {e+1}")
+        if cfg["topo"].is_root:
+            emulator.save_checkpoint(cfg["params"], id=e+1)
+
+    logging.info("Done Training")
+    trainer.shutdown(cancel=True)
+    validator.shutdown(cancel=True)
+
+    return cfg
+
+
+if __name__ == "__main__":
+    train(RemoteTrainer, PackedTrainer, missing_samples=[59419])
